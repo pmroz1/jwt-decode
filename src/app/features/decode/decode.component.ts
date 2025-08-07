0f1eb7609557ce9d05e5c9e7333220f18fa2f7d0
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-08-07 11:57:51 +0200

    feat: enhance DecodeComponent with button functionality for clearing and copying input

--- src/app/features/decode/decode.component.ts
+++ src/app/features/decode/decode.component.ts
@@ -4,15 +4,32 @@ import { exampleEncodedJwt } from '@app/shared/data/example-jwt.data';
 import { TagModule } from 'primeng/tag';
 import { TagData } from '@features/decode/models/tag.model';
 import { SeverityType } from './dictionaries/severity-type.dictionary';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-decode',
-  imports: [TextAreaComponent, TagModule],
+  imports: [TextAreaComponent, TagModule, ButtonModule],
   template: `
     <div class="flex flex-row items-center min-h-100 h-full">
-      <div class="flex flex-col w-1/2 h-full p-4">
+      <div class="flex flex-col w-full h-full p-4">
+        <div class="flex flex-row justify-between items-center ">
+          <p class="text-lg font-semibold">{{ inputHeader }}</p>
+          <div class="ml-4 flex flex-wrap gap-2">
+            <button pButton severity="secondary" (click)="clearInput()">
+              <i class="pi pi-eraser" pButtonIcon></i>
+              <span pButtonLabel>CLEAR</span>
+            </button>
+            <button pButton severity="secondary" (click)="copyToClipboard()">
+              <i class="pi pi-copy" pButtonIcon></i>
+              <span pButtonLabel>COPY</span>
+            </button>
+            @for(tag of inputTags(); track $index) {
+            <p-tag [value]="tag.value" [severity]="tag.severity"></p-tag>
+            }
+          </div>
+        </div>
         <app-text-area
-          class="w-full h-full p-4"
+          class="w-full h-full pt-4"
           [inputValue]="jwt()"
           [placeholder]="'Enter JWT here...'"
         ></app-text-area>
@@ -27,10 +44,22 @@ import { SeverityType } from './dictionaries/severity-type.dictionary';
 })
 export class DecodeComponent {
   jwt = signal<string>(exampleEncodedJwt.trim());
-  decodedJwt = signal<any>(null);
+  decodedJwt = signal<string>('');
+
+  inputHeader = 'JSON Web Token (JWT) Input:';
 
   inputTags = signal<TagData[]>([
-    { value: 'JWT', severity: SeverityType.Info },
-    { value: 'Decode', severity: SeverityType.Success },
+    // { value: 'JWT', severity: SeverityType.Info },
+    // { value: 'Decode', severity: SeverityType.Success },
   ]);
+
+  clearInput() {
+    // this.jwt.set('');
+    this.jwt.set('');
+    this.decodedJwt.set('');
+  }
+
+  copyToClipboard() {
+    
+  }
 }
